7e386671649375574a03b3693f598bece0553b55
Ivaylo Donchev <idonchev@hotmail.com>
2019-11-15 08:48:07 +0200

    erer

--- Lect_2/Lect_2/Source.cpp
+++ Lect_2/Lect_2/Source.cpp
@@ -2,24 +2,45 @@
 #include <cmath>				// за sin, cos, tan
 #include <iostream>
 using namespace std;
+const int n = 100;
 
 int main()
 {
-	double a, b;	//операнди
-	char op;		//знак за операция
-	cout << ">> ";
-	cin >> a >> op >> b;
-	switch (op)
-	{
-		case '+': cout << a + b; break;
-		case '-': cout << a - b; break;
-		case '*': cout << a * b; break;
-		case '/': if (b != 0) cout << a / b;
-				  else cout << "Division by zero!";
-				  break;
-		case '^': cout << pow(a, b); break;
-		default: cout << "Unknown operation";
-	}
+	double temp[] { 21.5, 22.0, 22.5, 21.0, 17.25 };
+	constexpr int N = sizeof(temp) / sizeof(double);	// пресмятане на размера на масива
+	for (int i = 0; i < N; i++)
+		cout << temp[i] << ' ';
 	cout << endl;
+	for (auto x : temp)
+		cout << x << ' ';
+	cout << endl;
+
+	for (int i = 0; i < N; i++)
+	{
+		cout << "temp[" << i << "] = ";
+		cin >> temp[i];
+	}
+	for (auto x : temp)
+		cout << x << ' ';
+	cout << endl;
+	for (auto& x : temp)
+	{
+		static int i{ 0 };
+		cout << i++ << ">> ";
+		cin >> x;
+	}
+	for (auto x : temp)
+		cout << x << ' ';
+	cout << endl;
+
+	/*cout << temp[0] << endl;
+	double average = (temp[0] + temp[1] + temp[2]) / 3.0;
+	swap(temp[2], temp[4]);
+	cout << "&temp[0] = " << &temp[0] << endl;
+	cout << "&temp[1] = " << &temp[1] << endl;
+	cout << "&temp[2] = " << &temp[2] << endl;
+	cout << reinterpret_cast<int>(&temp[2]) - reinterpret_cast<int>(&temp[1]) << endl;*/
+
+
 }
 
--- Lect_5/Source.cpp
+++ Lect_5/Source.cpp
@@ -1,22 +1,88 @@
-#include<iostream>
+#include <iostream>
+#include <string>
 using namespace std;
+
+template<typename T>
+T mult(T a, T b)
+{
+	return a * b;
+}
+
+template<typename T1, typename T2>
+auto mult(T1 a, T2 b)
+{
+	return a * b;
+}
+
+template<typename T, int N>
+class Stack
+{
+public:
+	bool Push(T x);
+	bool Pop(T& x);
+	Stack();
+private:
+	T buf[N];
+	int sp;
+};
+
+template<typename T, int N>
+Stack<T, N>::Stack()
+{
+	sp = -1;
+}
+
+template<typename T, int N>
+bool Stack<T, N>::Push(T x)
+{
+	if (sp >= N - 1) return false;
+	buf[++sp] = x;
+	return true;
+}
+
+template<typename T, int N>
+bool Stack<T, N>::Pop(T& x)
+{
+	if(sp<0) return false;
+	x = buf[sp--];
+	return true;
+}
 
 int main()
 {
-	const int n = 10;
-	int min = INT_MAX;	//най-голямото число от тип int
-	int max = INT_MIN;	//най-малкото число от тип int
-	int x;
-	for (int i = 1; i <= n; i++)
-	{
-		cout << "Enter a number: ";
-		cin >> x;
-		if (x < min) min = x;
-		if (x > max) max = x;
-	}
-	cout << "min = " << min << endl;
-	cout << "max = " << max << endl;
+	Stack<int, 10> st;
+	for (int i = 1; i <= 100; i++)
+		if(!st.Push(i))
+			cout << "The stack is full...\n";
+	int k;
+	if(st.Pop(k))
+		cout << "k = " << k << endl;
+	if (st.Pop(k))
+		cout << "k = " << k << endl;
+	if (st.Pop(k))
+		cout << "k = " << k << endl;
+	if (st.Pop(k))
+		cout << "k = " << k << endl;
+
+	Stack<double, 20> dblStack;
+	dblStack.Push(2.5);
+	dblStack.Push(1.25);
+	double d;
+	dblStack.Pop(d);
+	cout << "d = " << d << endl;
+
+	Stack<string, 3> s;
+	s.Push("Ivan");
+	s.Push("Stoyan");
+	s.Push("Gergana");
+	s.Push("Stefka");
+	string name;
+	while (s.Pop(name))
+		cout << name << " ";
+	cout << endl;
 } 
+
+
 
 
 
