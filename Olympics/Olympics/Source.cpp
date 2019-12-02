82b6070e58fe19eb814a506e739085774882f1c1
Ivaylo Donchev <idonchev@hotmail.com>
2019-12-02 18:06:56 +0200

    asdfasdf

--- Olympics/Olympics/Source.cpp
+++ Olympics/Olympics/Source.cpp
@@ -2,7 +2,7 @@
 #include <ctime>
 using namespace std;
 
-enum class Animal {Eagle, Snake, Mouse};
+enum Animal {Eagle, Snake, Mouse};
 
 size_t eagles{ 6 }, snakes{ 17 }, mouses{ 55 };
 
@@ -15,26 +15,26 @@ void Show()
 
 bool Eats(Animal predator, Animal victim)
 {
-	if (predator == Animal::Eagle && eagles > 0)		// хищникът е орел
+	if (predator == Eagle && eagles > 0)		// хищникът е орел
 	{
-		if (victim == Animal::Snake && snakes > 0)		// орелът яде змия
+		if (victim == Snake && snakes > 0)		// орелът яде змия
 		{
-			--eagles;						// намаляват орлите
-			--snakes;						// намаляват змиите
-			++mouses;						// увеличават се мишките (орелът става мишка)
+			--eagles;							// намаляват орлите
+			--snakes;							// намаляват змиите
+			++mouses;							// увеличават се мишките (орелът става мишка)
 			return true;
 		}
-		if (victim == Animal::Mouse && mouses > 0)	// орелът яде мишка
+		if (victim == Mouse && mouses > 0)		// орелът яде мишка
 		{
 			--eagles;
 			--mouses;
-			++snakes;		// орелът става змия
+			++snakes;							// орелът става змия
 			return true;
 		}
 		return false;
 	}
-	if (predator == Animal::Snake && snakes > 0 && mouses > 0)		// хищникът е змия и яде само мишки
-	{
+	if (predator == Snake && snakes > 0 && mouses > 0)
+	{		// хищникът е змия и яде само мишки
 			--snakes;
 			--mouses;
 			++eagles;		// змията става орел
@@ -49,11 +49,11 @@ int main()
 	Show();
 	size_t count{ 0 };
 	do {
-		Animal p = Animal(rand() % 2);
-		Animal v = Animal(1 + rand() % 2);
-		if(Eats(p, v))
+		auto predator = Animal(rand() % 2);
+		auto victim = Animal(1 + rand() % 2);
+		if (Eats(predator, victim))
 			++count;
 		Show();
-	} while (eagles+snakes+mouses > 1);
+	} while (eagles + snakes + mouses > 1);
 	cout << count << " eatings\n";
 }
