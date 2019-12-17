d1f635c338c5173ce5b5580adcec58ad96a473c9
Ivaylo Donchev <idonchev@hotmail.com>
2019-12-17 15:12:47 +0200

    r

--- Week_10/Week_10/Source.cpp
+++ Week_10/Week_10/Source.cpp
@@ -1,44 +1,48 @@
 #include<iostream>
 using namespace std;
 
-int sum(int, int);	// декларация на функция
-void show_message(const char*);
-double power(double, int);
-int max(int, int);
+double power( double x,  int n)
+{
+	double res = 1.0;
+	for (int i = 1; i <= n; i++)
+		res *= x;
+	return res;
+}
+
+void foo(int& x)
+{
+	x += 2;
+	cout << "x = " << x << endl;
+}
+
+void my_swap(int* x, int* y)
+{
+	int t = *x;
+	*x = *y;
+	*y = t;
+}
+
+int sum(int mas[], const int n)
+{
+	int s = 0;
+	for (int i = 0; i < n; i++)
+		s += mas[i];
+	return s;
+}
 
 int main()
 {
-	cout << sum(4, 5) << endl;
-	show_message("Hello!");
-	cout << power(2, 10) << endl;
-	cout << max(3, 4) << endl;
+	int A[]{ 4,6,78,3,1,3,5,7,4,2 };
+	int n = sizeof(A) / sizeof(A[0]);	// брой на елементите на масива
+	cout << sum(A, n) << endl;;
 
+	cout << "n = "; cin >> n;
+	int* p = new int[n];	// динамичен масив от n цели числа
+	for (int i = 0; i < n; i++)
+	{
+		p[i] = rand() % 100 + 1;
+		cout << p[i] << ' ';
+	}
+	cout << "\nsum(p) = " << sum(p, n) << endl;
+	delete[] p;
 }
-
-int sum(int a, int b)
-{
-	return a + b;
-}
-
-double power(double x, int n)		// заглавна част
-{									// от тук започва тялото
-	double p{ 1.0 };
-	for (int i = 1; i <= n; i++)
-		p *= x;
-	return p;						// връща резултат
-}
-
-void show_message(const char* s)
-{
-	cout << s << endl; 
-}
-
-int max(int a, int b)
-{
-	if (a > b)
-		return a;
-	else
-		return b;
-}
-
-
