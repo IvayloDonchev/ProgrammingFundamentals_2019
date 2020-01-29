6c8c9a39c1026d2c12367aacb1dead81fbe9e84b
Ivaylo Donchev <idonchev@hotmail.com>
2020-01-29 12:57:23 +0200

    dfsdf

---
+++ Exam/Exam/Source.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+	int examHours, examMinutes, arrivalHours, arrivalMinutes;
+	cin >> examHours;
+	cin >> examMinutes;
+	cin >> arrivalHours;
+	cin >> arrivalMinutes;
+	string late = "Late";
+	string onTime = "On time";
+	string early = "Early";
+	int examTime = (examHours * 60) + examMinutes;
+	int arrivalTime = (arrivalHours * 60) + arrivalMinutes;
+	int totalMinutesDifference = arrivalTime - examTime;
+	string studentArrival = late;
+	if (totalMinutesDifference < -30)
+		studentArrival = early;
+	else
+		if (totalMinutesDifference <= 0)
+			studentArrival = onTime;
+	string result;
+	if (totalMinutesDifference != 0)
+	{
+		int hoursDifference = abs(totalMinutesDifference / 60);
+		int minutesDifference = abs(totalMinutesDifference % 60);
+		if (hoursDifference > 0)
+			result = to_string(hoursDifference) + ":";
+		if (minutesDifference < 10)
+			result += "0";
+		result += to_string(minutesDifference) + " hours";
+	}
+	else
+		
+
+}
